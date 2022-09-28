405a8096ce52857fb8b08a81c84c53c584b76b71
sinkingpoint <colin@quirl.co.nz>
2022-09-28 16:40:09 +1000

    Initial RemoteReadHandler
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- cmd/promhouse/main.go
+++ cmd/promhouse/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"io"
+	"net/http"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/prometheus/prometheus/prompb"
+)
+
 func main() {
 
+}
+
+type ProtoPointer[T any] interface {
+	*T
+	proto.Message
+}
+
+var RemoteReadHandler = ProtoHandler[prompb.ReadRequest, prompb.ReadResponse]
+var RemoteWriteHandler = ProtoHandler[prompb.WriteRequest, prompb.ReadResponse]
+
+func ProtoHandler[Request any, Response any, RequestPtr ProtoPointer[Request], ResponsePtr ProtoPointer[Response]](handler func(req Request) (*Response, error)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		requestBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "failed to read request body", http.StatusBadRequest)
+			return
+		}
+
+		var readRequest Request
+		if err := proto.Unmarshal(requestBytes, RequestPtr(&readRequest)); err != nil {
+			http.Error(w, "failed to parse request body", http.StatusBadRequest)
+			return
+		}
+
+		readRequestResponse, err := handler(readRequest)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		responseBytes, err := proto.Marshal(ResponsePtr(readRequestResponse))
+		if err != nil {
+			http.Error(w, "failed to marshal response", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/protobuf")
+		w.WriteHeader(http.StatusOK)
+		w.Write(responseBytes)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/sinkingpoint/promhouse
 
 go 1.18
+
+require (
+	github.com/prometheus/prometheus v0.38.0
+	google.golang.org/protobuf v1.28.1
+)
+
+require github.com/gogo/protobuf v1.3.2 // indirect
