bcc166895201da33d512abad86300c888bd775af
sinkingpoint <colin@quirl.co.nz>
2022-10-13 19:07:35 +1100

    Test Gorilla Encoding

--- cmd/promhouse/sql/metrics.sql
+++ cmd/promhouse/sql/metrics.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS metrics (
     timestamp DateTime CODEC(Delta(4), ZSTD),
     name LowCardinality(String) CODEC(ZSTD),
     tags Map(String, String) CODEC(ZSTD),
-    value Float64 CODEC (Delta(4), ZSTD),
+    value Float64 CODEC (Gorilla, ZSTD),
 ) ENGINE = MergeTree() PRIMARY KEY (timestamp, name, tags);
